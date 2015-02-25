c35038264346c33276f5f148fdc89f6a98fa333a
Zachari Dahran <ZDahran@gmail.com>
2015-02-24 19:02:37 -0500

    Added in generating black cards for the starting deck

---
+++ Crossfire/Assets/DeckSetup.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class DeckSetup : MonoBehaviour {
+
+	public DeckActions MainDeck;
+	public GameObject OriginalCard;
+
+	// Use this for initialization
+	void Start () {
+		// Create the starting deck
+		if (MainDeck != null) { 
+			List<GameObject> cardList = new List<GameObject>();
+			for (int i = 0; i < 10; i++) {
+				cardList.Add(GameObject.Instantiate(OriginalCard) as GameObject);
+			}
+
+			MainDeck.AddCards(cardList);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
