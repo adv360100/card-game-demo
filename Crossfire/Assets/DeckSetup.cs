fc3c1b7a6fccf9b8bec2ad7bee1c7816d3e0c3f8
Zachari Dahran <ZDahran@gmail.com>
2015-02-24 19:20:47 -0500

    Changed Card prefab to Quad to fix upside down texture. Moved cards under the deck to avoid showing on the screen.

--- Crossfire/Assets/DeckSetup.cs
+++ Crossfire/Assets/DeckSetup.cs
@@ -13,7 +13,12 @@ public class DeckSetup : MonoBehaviour {
 		if (MainDeck != null) { 
 			List<GameObject> cardList = new List<GameObject>();
 			for (int i = 0; i < 10; i++) {
-				cardList.Add(GameObject.Instantiate(OriginalCard) as GameObject);
+				GameObject cardToAdd = GameObject.Instantiate(OriginalCard, MainDeck.transform.position, MainDeck.transform.rotation) as GameObject;
+
+				Vector3 pos = cardToAdd.transform.position;
+				pos.z = 1;
+				cardToAdd.transform.position = pos;
+				cardList.Add(cardToAdd);
 			}
 
 			MainDeck.AddCards(cardList);
