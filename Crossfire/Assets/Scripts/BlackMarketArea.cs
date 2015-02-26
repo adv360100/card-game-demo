4074fe5fd1782db92050a2c4e018fcfb39e075b8
Andrew Vandeyar <warpstar360100@yahoo.com>
2015-02-26 13:13:52 -0800

    first pass at the black market area

---
+++ Crossfire/Assets/Scripts/BlackMarketArea.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class BlackMarketArea : BasicAnimator {
+
+	public List<GameObject> CardList;
+	public int Columns = 3;
+	public int Rows = 2;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	override public void AnimateTarget(GameObject target, float rate) {
+		CardList.Add (target);
+		UpdateDeckDisplay (rate);
+	}
+
+	void UpdateDeckDisplay(float animationRate)
+	{
+		float objectWidth = CardList [0].renderer.bounds.size.x;
+		float objectHeight = CardList [0].renderer.bounds.size.y;
+		int count = Columns - 1;
+		float containerWidth = count * objectWidth;
+		count = Rows - 1;
+		float containerHeight = Rows * objectHeight;
+		int index = 0;
+		//find start point
+		float startPointX = containerWidth * -0.5f;
+		float startPointY = containerHeight * -0.5f;
+		foreach (GameObject cardObject in CardList) 
+		{
+			Card card = cardObject.GetComponent<Card>();
+			float x = startPointX + objectWidth * (index / Rows);
+			float y = startPointY + objectHeight * (index / Columns);
+			card.AnimateTo(new Vector3(x, y, transform.position.z),animationRate);
+			index++;
+		}
+	}
+}
--- Crossfire/Assets/Scripts/DeckSetup.cs
+++ Crossfire/Assets/Scripts/DeckSetup.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 public class DeckSetup : MonoBehaviour {
 
 	public DeckAnimator MainDeck;
+	public DeckAnimator MarketDeck;
 	public GameObject OriginalCard;
 
 	// Use this for initialization
@@ -23,8 +24,27 @@ public class DeckSetup : MonoBehaviour {
 
 			MainDeck.AddCards(cardList);
 		}
+
+		InitBlackMarket ();
 	}
-	
+
+	void InitBlackMarket()
+	{
+		if (MainDeck != null) { 
+			List<GameObject> cardList = new List<GameObject>();
+			for (int i = 0; i < 5; i++) {
+				GameObject cardToAdd = GameObject.Instantiate(OriginalCard, MainDeck.transform.position, MainDeck.transform.rotation) as GameObject;
+				
+				Vector3 pos = cardToAdd.transform.position;
+				pos.z = 1;
+				cardToAdd.transform.position = pos;
+				cardList.Add(cardToAdd);
+			}
+			
+			MarketDeck.AddCards(cardList);
+		}
+	}
+
 	// Update is called once per frame
 	void Update () {
 	
