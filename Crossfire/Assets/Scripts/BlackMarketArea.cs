a149366b8aa7948fe05897fbb8d247aebc789236
Andrew Vandeyar <warpstar360100@yahoo.com>
2015-02-26 17:34:27 -0800

    removed unit test stuff. made player and market decks

--- Crossfire/Assets/Scripts/BlackMarketArea.cs
+++ Crossfire/Assets/Scripts/BlackMarketArea.cs
@@ -5,22 +5,23 @@ using System.Collections.Generic;
 public class BlackMarketArea : BasicArea {
 
 	public Deck MarketDeck;
-	public Deck DiscardDeck;
+	public DiscardDeck DiscardDeck;
 	public GameObject BasicCard;
 	public List<GameObject> CardList; //cards in the market area
 	public int Columns = 3;
 	public int Rows = 2;
+	public int CardMax = 6;
 
 	// Use this for initialization
 	void Start () {
 		if (MarketDeck != null)
 		{
 			List<GameObject> cardList = new List<GameObject>();
-			for (int i = 0; i < 5; i++) {
+			for (int i = 0; i < 10; i++) {
 				GameObject cardToAdd = GameObject.Instantiate(BasicCard, MarketDeck.transform.position, MarketDeck.transform.rotation) as GameObject;
 				
 				Vector3 pos = cardToAdd.transform.position;
-				pos.z = 1;
+				pos.z = float.MaxValue;
 				cardToAdd.transform.position = pos;
 				cardList.Add(cardToAdd);
 			}
@@ -28,10 +29,36 @@ public class BlackMarketArea : BasicArea {
 			MarketDeck.AddCards(cardList);
 		}
 	}
+
+	void Update()
+	{
+		bool needsUpdate = false;
+		while (CardList.Count < CardMax)
+		{
+			GameObject card = MarketDeck.DrawCard();
+			if(card == null)
+				break;
+			needsUpdate = true;
+			card.transform.position = transform.position;
+			CardList.Add(card);
+		}
+
+		if (needsUpdate)
+			UpdateDeckDisplay (1f);
+	}
 	
-	// Update is called once per frame
-	void Update () {
-	
+	public List<GameObject> GetDiscardPile () {
+		return DiscardDeck.CardList;
+	}
+
+	public void RemoveDiscardPile () {
+		Vector3 curPos = DiscardDeck.transform.position;
+		DiscardDeck.AnimateTo(MarketDeck.transform.position, 1.0f, p => {
+			DiscardDeck.RemoveAllCards();
+			DiscardDeck.UpdateDeckDisplay();
+			DiscardDeck.transform.position = curPos;
+			return 0;
+		});
 	}
 
 	override public void OnFocusEnter()
