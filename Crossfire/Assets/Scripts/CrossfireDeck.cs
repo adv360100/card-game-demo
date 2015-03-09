e6b5f44b67317545de542485f0c01e311f755ff5
Andrew Vandeyar <warpstar360100@yahoo.com>
2015-03-09 15:35:32 -0700

    can draw from the crossfire deck. added long card prefab. added crossfire deck script

---
+++ Crossfire/Assets/Scripts/CrossfireDeck.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CrossfireDeck : Deck {
+
+	public BasicArea AreaManager;
+
+	void OnMouseDown()
+	{
+		if (CardList.Count <= 0) {
+			return;
+		}
+		
+		GameObject topCard = CardList [CardList.Count - 1];
+		topCard.GetComponent<Card>().CurrentCardLocation = CardLocation.CardLocationCrossfireDeck;
+		topCard.GetComponent<Renderer>().enabled = true;
+		CardList.Remove (topCard);
+
+		topCard.GetComponent<Card>().QuadraticOutMoveTo(topCard.transform.position,
+		                                                AreaManager.DiscardPile.transform.position,
+		                                                1f,
+		                                                ()=>{
+			AreaManager.DiscardPile.AddCard(topCard);
+			topCard.transform.parent = AreaManager.DiscardPile.transform;											
+		});
+	}
+}
