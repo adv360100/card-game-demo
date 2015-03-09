a48ce5d71ab0f31b9aec0d74cd5ab37c1ea7d92b
Zachari Dahran <ZDahran@gmail.com>
2015-03-09 17:19:15 -0400

    Creating obstacle area for player manager

---
+++ Crossfire/Assets/Scripts/ObstacleDeck.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObstacleDeck : Deck {
+
+	public GameManager MyGameManager;
+
+	void OnMouseDown()
+	{
+		GameObject card = DrawCard ();
+		if (card == null) {
+			return;
+		}
+
+		card.transform.position = transform.position;
+		MyGameManager.AddObstacleToMyPlayer (card);
+		
+		UpdateDeckDisplay ();
+	}
+}
