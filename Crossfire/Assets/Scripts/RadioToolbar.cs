0290d07a566c1e8025b99f1f6de79ba082902f21
Andrew Vandeyar <warpstar360100@yahoo.com>
2015-03-10 16:16:03 -0700

    added some todos. added autolayout to navbar. radio toolbar show hides selected button.

---
+++ Crossfire/Assets/Scripts/RadioToolbar.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class RadioToolbar : MonoBehaviour {
+
+	public Button SelectedButton;
+
+	// Use this for initialization
+	void Start () {
+		SelectedButton.gameObject.SetActive(false);
+	}
+
+	public void SelectButton(Button btn)
+	{
+		SelectedButton.gameObject.SetActive (true);
+		SelectedButton = btn;
+		btn.gameObject.SetActive (false);
+	}
+}
