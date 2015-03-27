ba8713258fbb43cb828e72791f6e2f1fe935790c
Zachari Dahran <ZDahran@gmail.com>
2015-03-27 19:31:37 -0400

    Added all cards to the game. Added support for pulling cards from the black market deck and adding them to a player's deck.

--- Crossfire/Assets/Scripts/ObstacleArea.cs
+++ Crossfire/Assets/Scripts/ObstacleArea.cs
@@ -5,22 +5,19 @@ using System.Collections.Generic;
 public class ObstacleArea : BasicArea {
 	public Deck HardDeck;
 
-	override public void Start () {
-		base.Start ();
-		if (HardDeck != null) {
-			List<GameObject> cardList = new List<GameObject>();
-			for (int i = 0; i < 10; i++) {
-				Vector3 newPos = new Vector3(MainDeck.transform.position.x, MainDeck.transform.position.y, MainDeck.transform.position.z + 1);
-				GameObject cardToAdd = GameObject.Instantiate(BasicCard, newPos, MainDeck.transform.rotation) as GameObject;
-				//parent card for easier debugging
-				cardToAdd.transform.parent = MainDeck.transform;
-				cardToAdd.GetComponent<Card>().AreaManager = this;
-				cardToAdd.GetComponent<Renderer>().enabled = false;
-				cardList.Add(cardToAdd);
-			}
-			
-			HardDeck.AddCards(cardList);
-			HardDeck.UpdateDeckDisplay();
-		}
+	public void SetHardDeck (List<GameObject> cards) {
+		SetDeck (cards, HardDeck);
+	}
+
+	public void ShuffleHardDeck () {
+		HardDeck.Shuffle ();
+	}
+
+	public uint[] GetHardDeckOrder () {
+		return GetDeckOrder (HardDeck);
+	}
+
+	public void SetHardDeckOrder (uint[] order) {
+		SetDeckOrder (HardDeck, order);
 	}
 }
