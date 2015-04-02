caabf87e897cc7c705b2c9ead1002fd69afa2e35
Zachari Dahran <ZDahran@gmail.com>
2015-04-02 19:56:43 -0400

    Unused player buttons are now removed.

--- Crossfire/Assets/Scripts/RadioToolbar.cs
+++ Crossfire/Assets/Scripts/RadioToolbar.cs
@@ -5,14 +5,18 @@ using UnityEngine.UI;
 public class RadioToolbar : MonoBehaviour {
 
 	public Button SelectedButton;
+	public Button[] PlayerButtons;
 
 	// Use this for initialization
 	void Start () {
-		SelectedButton.gameObject.SetActive(false);
+		SelectedButton.gameObject.SetActive (false);
+
+		for (int i = PersistantManager.GetInstance ().Players.Count; i < PlayerButtons.Length; i++) {
+			PlayerButtons[i].gameObject.SetActive (false);
+		}
 	}
 
-	public void SelectButton(Button btn)
-	{
+	public void SelectButton (Button btn) {
 		SelectedButton.gameObject.SetActive (true);
 		SelectedButton = btn;
 		btn.gameObject.SetActive (false);
